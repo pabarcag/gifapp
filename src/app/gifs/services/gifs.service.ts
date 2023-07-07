2840e7a35935e08c8bd2526af4846414370b6f53
Patricio Abarca <patricioabarca88@gmail.com>
2023-07-07 14:55:07 -0400

    se agrega la peticion http

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,10 +1,11 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 @Injectable({ providedIn: 'root' })
 export class GifsService {
-    constructor() {}
+    constructor(private http: HttpClient) {}
 
     private gyphyAPIKey = 'MYTnVkK1UOCwEZ5LMVyE37bxwIy26a2P';
-
+    private serviceUrl = 'https://api.giphy.com/v1/gifs';
     private _tagHistory: string[] = [];
 
     get getTagHistory(): string[] {
@@ -28,6 +29,14 @@ export class GifsService {
     searchTag(tag: string): void {
         if (tag.length === 0) return;
         this.organizeHistory(tag);
-        // this._tagHistory.unshift(tag);
+
+        const params = new HttpParams()
+            .set('api_key', this.gyphyAPIKey)
+            .set('limit', '10')
+            .set('q', tag);
+
+        this.http
+            .get(`${this.serviceUrl}/search`, { params })
+            .subscribe((res) => console.log(res));
     }
 }
